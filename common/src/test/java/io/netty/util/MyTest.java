295d7f997dc193a67fa6b801f6acbf3adc078071
bairen <bairen@wacai.com>
2023-05-04 20:08:58 +0800

    modify README.md add MyTest

---
+++ common/src/test/java/io/netty/util/MyTest.java
@@ -0,0 +1,40 @@
+package io.netty.util;
+
+import io.netty.util.RecyclerTest.HandledObject;
+import org.junit.Test;
+
+/**
+ * @author bairen
+ * @description
+ **/
+public class MyTest {
+
+    @Test
+    public void test() {
+        Recycler<HandledObject>  recycler = newRecycler();
+        recycler.get();
+        System.out.println("========================");
+
+    }
+
+    private static Recycler<HandledObject> newRecycler() {
+        return new Recycler<HandledObject>() {
+            @Override
+            protected HandledObject newObject(Handle<HandledObject> handle) {
+                return new HandledObject(handle);
+            }
+        };
+    }
+
+    static final class HandledObject {
+        Recycler.Handle<HandledObject> handle;
+
+        HandledObject(Recycler.Handle<HandledObject> handle) {
+            this.handle = handle;
+        }
+
+        void recycle() {
+            handle.recycle(this);
+        }
+    }
+}
